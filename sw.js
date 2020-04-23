55f08b7d4673b23ff37adaa92989944496eb6f23
ToonicTie <62765705+ToonicTie@users.noreply.github.com>
2020-04-23 13:58:29 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+var deferredPrompt;
+self.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('first-app')
+      .then(function(cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/css/app.css',
+          '/src/js/app.js'
+        ])
+      })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        return res;
+      })
+  );
+});
